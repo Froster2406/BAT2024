69fc0c1b52b6adc9848285b027e281e0cdd5d647
Stefano Nicora <stefano.nicora@hslu.ch>
2024-04-02 15:55:19 +0200

    Add basic file for GPIO-only manipulation

---
+++ Software/custom/BAT_GPIO.c
@@ -0,0 +1,17 @@
+/*
+ * BAT_GPIO.c
+ *
+ *  Created on: 02.04.2024
+ *      Author: FrosterOTG
+ */
+
+#include "BAT_GPIO.h"
+
+void BAT_GPIO_init(void){
+  // Enable GPIO clock
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  /* Configure GPIO pin 2 on port D as input */
+  /* used to check if device is charging */
+  GPIO_PinModeSet(BAT_CHARGING_PORT, BAT_CHARGING_PIN, gpioModeInput, 0);
+}
---
+++ Software/custom/BAT_GPIO.h
@@ -0,0 +1,19 @@
+/*
+ * BAT_GPIO.h
+ *
+ *  Created on: 02.04.2024
+ *      Author: FrosterOTG
+ */
+
+#ifndef CUSTOM_BAT_GPIO_H_
+#define CUSTOM_BAT_GPIO_H_
+
+#include "em_gpio.h"
+#include "em_cmu.h"
+#include "em_chip.h"
+
+#include "BAT_DEFINES.h"
+
+void BAT_GPIO_init(void);
+
+#endif /* CUSTOM_BAT_GPIO_H_ */
