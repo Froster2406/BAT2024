b1ce15d7839464c048cc07f3c269c33cc373e8f8
Stefano Nicora <stefano.nicora@hslu.ch>
2024-04-29 17:55:02 +0200

    Add logging string function

--- Software/custom/BAT_LOGGING.c
+++ Software/custom/BAT_LOGGING.c
@@ -5,4 +5,38 @@
  *      Author: FrosterOTG
  */
 
+#include "BAT_LOGGING.h"
 
+/***************************************************************************//**
+ * @brief
+ *   Generate a full string that will be stored in memory
+ *
+ * @param[out] outputStr
+ *   Data-array that holds the complete string
+ *   Has to able to hold 16 items
+ *
+ * @param[in] timestamp
+ *   Data-array that holds the timestamp as a string
+ *
+ * @param[in] dBSPL
+ *   Data-array that holds the dBSPL value as a string
+ *
+ ******************************************************************************/
+void BAT_LOGGING_generateLogString(char* outputStr, char* timestamp, char *dBSPL){
+  outputStr[0] = timestamp[0];
+  outputStr[1] = timestamp[1];
+  outputStr[2] = timestamp[2];
+  outputStr[3] = timestamp[3];
+  outputStr[4] = timestamp[4];
+  outputStr[5] = timestamp[5];
+  outputStr[6] = timestamp[6];
+  outputStr[7] = timestamp[7];
+  outputStr[8] = '_';
+  outputStr[9] = dBSPL[0];
+  outputStr[10] = dBSPL[1];
+  outputStr[11] = dBSPL[2];
+  outputStr[12] = dBSPL[3];
+  outputStr[13] = dBSPL[4];
+  outputStr[14] = dBSPL[5];
+  outputStr[15] = '\0';
+}
--- Software/custom/BAT_LOGGING.h
+++ Software/custom/BAT_LOGGING.h
@@ -8,6 +8,8 @@
 #ifndef CUSTOM_BAT_LOGGING_C_
 #define CUSTOM_BAT_LOGGING_C_
 
+#include <string.h>
 
+void BAT_LOGGING_generateLogString(char* outputStr, char* timestamp, char *dBSPL);
 
 #endif /* CUSTOM_BAT_LOGGING_C_ */
